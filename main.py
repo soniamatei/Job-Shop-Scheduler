b8138651e32da06b1640b3aa7a9f911adb317e21
914-Matei-Sonia <sonia.matei1@stud.ubbcluj.ro>
2023-03-18 21:25:25 +0200

    changed representation of arcs and reader class

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import reader
+from reader import Reader
 from scheduler import *
 
 
 def main():
-    machines, parts = reader.read_file("Input_Two.txt")
-    for m in machines:
-        print(m)
-    for p in parts:
-        print(p)
+    reader = Reader()
+    machines, parts = reader.read_file("Input_One.txt")
 
-    # scheduler = Scheduler(machines, parts)
-    # scheduler.create_conjunctive_arcs()
-    # scheduler.create_disjunctive_arcs()
+    scheduler = Scheduler(machines, parts)
+    scheduler.create_conjunctive_arcs()
+    scheduler.create_disjunctive_arcs()
     # print(scheduler.get_overall_time())
+    scheduler.print_machine_executions()
 
 
 if __name__ == '__main__':
--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,9 @@
 
-
 class Vertex:
 
     def __init__(self, part=None, operation=None, start_time=None, item_number=None):
         self.operation = operation
-        self.start = start_time
+        self.start = start_time  # start time on the conjunctive part
         self.part = part
         self.item_number = item_number
 
@@ -21,110 +20,137 @@ class Scheduler:
         self.machines = machines
         self.parts = parts
         self.vertices = []
-        self.edges = []
+        self.out = {}
         self.start = Vertex()
         self.finish = Vertex()
 
-    def create_conjunctive_arcs(self):
+    @staticmethod
+    def get_operation_time(vertex):
+        """
+        Calculate the time needed to process an operation.
+        :param vertex: Vertex
+        :return: integer
+        """
+        return vertex.start + vertex.operation.get_duration() + vertex.operation.get_machine().get_cooldown()
 
+    def create_conjunctive_arcs(self):
+        """
+        Create arcs between the operations on the same part.
+        """
+        self.out[self.start] = []
         for part in self.parts:
             # introduce as many 'rows' in the graph as many items of one part are there
             for item_number in range(part.get_no_items()):
                 operations = part.get_operations()
 
-                prev = self.start
                 # a variable which sums up the duration of each operation
                 # -> this will determine the starting time of each operation
                 accumulated_time = 0
-                for op in operations:
+
+                prev = Vertex(part, operations[0], accumulated_time, item_number)
+                self.vertices.append(prev)
+                self.out[self.start].append(prev)
+
+                for op in operations[1:]:
                     current = Vertex(part, op, accumulated_time, item_number)
 
                     self.vertices.append(current)
-                    self.edges.append((prev, current))
+                    self.out[prev] = []
+                    self.out[prev].append(current)
 
                     accumulated_time += op.get_duration()
                     prev = current
 
-                self.edges.append((prev, self.finish))
-
-        # for v in self.vertices:
-        #     print(v)
+                self.out[prev] = []
 
     def create_disjunctive_arcs(self):
-
+        """
+        Create arcs between operations on the same machine.
+        """
         for machine in self.machines:
-            vertices_by_machine = []
-
             # get all operations on a specific machine in one list
-            for vertex in self.vertices:
-                if vertex.operation.get_machine() == machine:
-                    vertices_by_machine.append(vertex)
+            vertices_by_machine = list(filter(lambda x: x.operation.get_machine() == machine, self.vertices))
 
             # make the specific connections between them as follows:
             # t = starting time of the op
             # p = processing time of the op
             # c = cooldown time of the machine
-            # t_i + p_i + c <= t_(i+1) + p_(i+1) + c or t_(i+1) + p_(i+1) + c <=  t_i + p_i + c
+            # t_i + p_i + c <= t_(i+1) + p_(i+1) + c  or  t_(i+1) + p_(i+1) + c <=  t_i + p_i + c
             for index1 in range(len(vertices_by_machine)):
                 for index2 in range(index1 + 1, len(vertices_by_machine)):
 
                     vertex1 = vertices_by_machine[index1]
                     vertex2 = vertices_by_machine[index2]
 
-                    if vertex1 != vertex2:
+                    if self.get_operation_time(vertex1) <= self.get_operation_time(vertex2):
+                        self.out[vertex1].append(vertex2)
 
-                        if vertex1.start + vertex1.operation.get_duration() + machine.get_cooldown() <= \
-                                vertex2.start + vertex2.operation.get_duration() + machine.get_cooldown():
-                            self.edges.append((vertex1, vertex2))
+                    elif self.get_operation_time(vertex2) <= self.get_operation_time(vertex1):
+                        self.out[vertex2].append(vertex1)
 
-                        elif vertex2.start + vertex2.operation.get_duration() + machine.get_cooldown() <= \
-                                vertex1.start + vertex1.operation.get_duration() + machine.get_cooldown():
-                            self.edges.append((vertex2, vertex1))
-
-        # for e in self.edges:
-        #     print(e[0], "|", e[1])
-
-    def calculate_length(self, path):
+    def print_machine_executions(self):
         """
-        Calculates the length (duration of operation + machine cooldown) of a path given.
-        :param path: list with vertices
-        :return: integer
+        Print the machines execution.
         """
-        length = 0
-        for vertex in path:
-            if vertex.part is not None:
-                length += vertex.operation.get_duration() + vertex.operation.get_machine().get_cooldown()
+        finish_times = {self.start: 0}
 
-        return length
+        for machine in self.machines:
+            # get all operations on the machine and order them after outdegree (the first to run is the one with
+            # the biggest outdegree
+            ops_on_machine = list(filter(lambda x: x.operation.get_machine() == machine, self.vertices))
+            ops_on_machine = sorted(ops_on_machine, key=lambda x: len(self.out[x]), reverse=True)
 
-    def find_path(self, source):
-        """
-        Algorithm for finding the longest path in a directed graph. A DFS based algorithm which selects
-        the longest path found from the adjacent nodes of the source.
+            current_operation = ops_on_machine[0]
+            parent_of_first_op = list(filter(lambda x: current_operation in self.out[x], self.out.keys()))[0]
 
-        :param source: vertex
-        :return: list of vertices
-        """
+            # initiate the starting time (how long one machine waits until it does the first operation)
+            # = the finish time of the parent of the first vertex operation
 
-        longest_path = []
-        adjacent_nodes = list(filter(lambda x: (source, x) in self.edges, self.vertices))
+            start = finish_times[parent_of_first_op]
+            cooldown = machine.get_cooldown()
+            # add the initial time and duration to find the finish time for the current operation
+            finish_times[current_operation] = start + current_operation.operation.get_duration()
 
-        for next in adjacent_nodes:
-            new_path = self.find_path(next)
+            print(f"{machine.get_name()}:   ", end="")
+            print(f"{current_operation.part.get_name()} ({current_operation.item_number})  start: {start}  "
+                  f"duration: {current_operation.operation.get_duration()}"
+                  f"  finish: {finish_times[current_operation]}s", end=" | ")
 
-            if new_path:
-                # if we find a new path
-                if not longest_path or self.calculate_length(new_path) > self.calculate_length(longest_path):
-                    longest_path = new_path
+            # set the finish time for each operation on the specific machine
+            for next in ops_on_machine[1:]:
 
-        longest_path.insert(0, source)
-        return longest_path
+                # next operation begins when current one ends
+                # start += current_operation.operation.get_duration()
+                start = finish_times[current_operation]
 
-    def get_overall_time(self):
-        """
-        Returns the total time of execution of all operations.
-        :return: integer
-        """
-        return self.calculate_length(self.find_path(self.start))
+                conjunctive_parent = list(filter(lambda x: next in self.out[x] and x.part == next.part and
+                                                           x.item_number == next.item_number, self.vertices))
 
-# TODO: machine schedule (first vertex without indegree)
+                # if the conj parent is not the source node
+                if conjunctive_parent:
+                    conjunctive_parent = conjunctive_parent[0]
+
+                    # calculate how long a machine waits before receiving another part
+                    diff = finish_times[conjunctive_parent] - finish_times[current_operation]
+
+                    # if is >0 add it to the time before the beginning of next operation
+                    if diff > 0:
+                        start += diff
+
+                        # see if the cooldown of the machine falls within the time machine has to wait for another part
+                        if cooldown - diff > 0:
+                            start += cooldown - diff
+                    else:
+                        start += cooldown
+                else:
+                    start += cooldown
+
+                finish_times[next] = start + next.operation.get_duration()
+
+                print(f"{next.part.get_name()} ({next.item_number})  start: {start}  "
+                      f"duration: {next.operation.get_duration()}"
+                      f"  finish: {finish_times[next]}s", end=" | ")
+
+                current_operation = next
+
+            print()
